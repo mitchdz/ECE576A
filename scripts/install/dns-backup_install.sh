f8cd49a2db96455c33aeea4324a1dc003a25841f
mitchdz <mitch_dz@hotmail.com>
2020-12-11 22:12:10 -0700

    fixed dhcp

--- scripts/install/dns-backup_install.sh
+++ scripts/install/dns-backup_install.sh
@@ -3,4 +3,4 @@ sudo apt-get update -y
 sudo apt-get install -y bind9 heartbeat isc-dhcp-server
 
 ## install rootfs
-cp -r /vagrant/rootfs/dns_rootfs/* /
+cp -r /vagrant/rootfs/dns-backup_rootfs/* / &> /dev/null || :
--- scripts/install/webserver_install.sh
+++ scripts/install/webserver_install.sh
@@ -3,4 +3,4 @@ sudo apt-get update -y
 sudo apt-get install -y net-tools apache2
 
 ## install rootfs
-cp -r /vagrant/rootfs/webserver_rootfs/* /
+cp -r /vagrant/rootfs/webserver_rootfs/* / &> /dev/null || :
